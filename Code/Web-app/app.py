071b7626fd793e9cba015928a5ba29e1d4c88339
slim-s <78751030+slim-s@users.noreply.github.com>
2023-07-28 22:56:56 +0530

    web

--- Code/Web-app/app.py
+++ Code/Web-app/app.py
@@ -1,104 +1,94 @@
-import dash
-from dash import dcc, html, Input, Output, State
-from dash.exceptions import PreventUpdate
-import pandas as pd
 import os
-import base64
-import io  # Correct import for BytesIO
+import pandas as pd
+import numpy as np
+from flask import Flask, render_template, request, redirect, url_for, send_file
+from tensorflow.keras.models import load_model
+from sklearn.preprocessing import MinMaxScaler
 
-app = dash.Dash(__name__)
+app = Flask(__name__)
 
-app.layout = html.Div([
-    html.H1("Upload Excel Files", style={'textAlign': 'center'}),
-    dcc.Upload(
-        id='upload-data',
-        children=html.Button('Click here to Vibration data files'),
-        style={
-            'width': '50%',
-            'height': '100px',
-            'lineHeight': '100px',
-            'borderWidth': '1px',
-            'borderStyle': 'dashed',
-            'borderRadius': '5px',
-            'textAlign': 'center',
-            'margin': 'auto'
-        },
-        multiple=True  # Allow multiple files to be uploaded
-    ),
-    dcc.Upload(
-        id='upload-exp',
-        children=html.Button('Click here to Upload experiment data File'),
-        style={
-            'width': '50%',
-            'height': '100px',
-            'lineHeight': '100px',
-            'borderWidth': '1px',
-            'borderStyle': 'dashed',
-            'borderRadius': '5px',
-            'textAlign': 'center',
-            'margin': 'auto'
-        }
-    ),
-    html.Div(id='output-data')
-])
+# Load the ML model
+model = load_model('cnn.h5')
+
+# Store the uploaded experiment and vibration files
+experiment_df = None
+vibration_dataframes = []
 
 
-@app.callback(
-    Output('output-data', 'children'),
-    Input('upload-data', 'contents'),
-    State('upload-data', 'filename')
-)
-def update_output(contents_list, filename_list):
-    if contents_list is None or len(contents_list) == 0:
-        raise PreventUpdate
+@app.route('/')
+def index():
+    return render_template('index.html', predictions=None)
 
-    all_dfs = []  # To store all the DataFrames from the uploaded files
 
-    for content, filename in zip(contents_list, filename_list):
-        if content is not None:
-            content_type, content_string = content.split(',')
-            try:
-                if 'csv' in filename:
-                    # Read the file using pandas
-                    df = pd.read_csv(io.StringIO(base64.b64decode(content_string).decode('utf-8')))
-                elif 'xls' in filename or 'xlsx' in filename:
-                    # Read the file using pandas
-                    df = pd.read_excel(io.BytesIO(base64.b64decode(content_string)))
-                else:
-                    return "Unsupported file format"
-            except Exception as e:
-                print(e)
-                return "Error processing file"
+@app.route('/upload_experiment', methods=['POST'])
+def upload_experiment():
+    global experiment_df
+    experiment_file = request.files['experiment_file']
+    if experiment_file:
+        # Process the experiment data
+        experiment_df = pd.read_excel(experiment_file)
+        return redirect(url_for('index'))
 
-            # Process the DataFrame (if needed)
+
+@app.route('/upload_vibration', methods=['POST'])
+def upload_vibration():
+    global vibration_dataframes
+    vibration_files = request.files.getlist('vibration_files')
+    if vibration_files:
+        for file in vibration_files:
+            # Process the vibration data
+            df = pd.read_excel(file)
             df = df.dropna(axis='columns', how='all')
             df = df.dropna(axis='rows', how='all')
             df.columns = ['Time', 'X', 'Y', 'Z']
             df = df.iloc[1:]
             df['Time'] = pd.to_datetime(df['Time'], unit='s').dt.time
+            df['X'] = pd.to_numeric(df['X'], errors='coerce')
+            df['Y'] = pd.to_numeric(df['Y'], errors='coerce')
+            df['Z'] = pd.to_numeric(df['Z'], errors='coerce')
+            df['Magnitude'] = np.sqrt(df['X']**2 + df['Y']**2 + df['Z']**2)
 
-            # Your additional DataFrame processing code here
-            # ...
+            # Add experiment details from experiment_df based on experiment number
+            experiment_number = int(df['Experiment'].iloc[0])  # Assuming 'Experiment' is a column in df
+            if experiment_df is not None:
+                experiment_row = experiment_df[experiment_df['Experiment'] == experiment_number]
+                if not experiment_row.empty:
+                    for column in experiment_row.columns[1:]:
+                        df[column] = experiment_row[column].values[0]
 
-            all_dfs.append(df)
+            vibration_dataframes.append(df)
+            print(f"Processed: {file.filename}")
+
+        return redirect(url_for('index'))
 
 
-'''
-    # Now you have all the DataFrames in the `all_dfs` list
-    # You can store them in the `dataframes` list or process them further
+@app.route('/predict', methods=['GET'])
+def predict():
+    global vibration_dataframes
+    if not vibration_dataframes:
+        return redirect(url_for('index'))
 
-    # For example, you can print the first few rows of each DataFrame:
+    # Concatenate all vibration dataframes into merged_df
+    merged_df = pd.concat(vibration_dataframes, ignore_index=True)
+    merged_df['Time'] = [(t.hour * 3600 + t.minute * 60 + t.second + t.microsecond / 1e6) for t in merged_df['Time']]
 
+    # Prepare the data for prediction
+    scaler = MinMaxScaler()
+    X_test_scaled = scaler.fit_transform(merged_df.drop(['Time', 'Magnitude'], axis=1))
 
-    output_children = []
-    for idx, df in enumerate(all_dfs):
-        output_children.append(html.Div([
-            html.H3(f"Uploaded File ({idx + 1}): {filename_list[idx]}"),
-            html.P(df.head().to_html())
-        ]))
+    # Make predictions using the loaded model
+    predictions = model.predict(np.expand_dims(X_test_scaled, axis=-1))
+    merged_df['Predictions'] = predictions
 
-    return output_children
-'''
+    # Save the predictions to an Excel file
+    predictions_file = 'predictions.xlsx'
+    merged_df.to_excel(predictions_file, index=False)
+
+    # Clear vibration_dataframes for next prediction
+    vibration_dataframes = []
+
+    return send_file(predictions_file, as_attachment=True)
+
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run(debug=True)
